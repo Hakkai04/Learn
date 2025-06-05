48ce3ccd6e03980db3c85138129a1632fd1d39e7
Hakkai <icyzhao04@gmail.com>
2025-06-05 23:02:56 +0800

    2025-06-05

---
+++ code/C++/acw861.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n1, n2, m;
+int u, v;
+
+int main()
+{
+    cin >> n1 >> n2 >> m;
+
+    while(m--)
+    {
+        cin >> u >> v;
+
+    }
+    return 0;
+}
